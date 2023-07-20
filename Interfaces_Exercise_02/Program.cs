754a805073c0137f321b413400be39b429f6135a
johndward01 <johndward01@gmail.com>
2023-07-20 11:39:36 -0500

    Add project files.

---
+++ Interfaces_Exercise_02/Program.cs
@@ -0,0 +1,11 @@
+﻿// TODO: Create an interface called IMediaPlayer
+// TODO: Inside of the interface make a method called Play()
+// TODO: Inside of the interface make a method called Pause()
+// TODO: Inside of the interface make a method called Stop()
+
+// TODO: Create 2 classes that implement the interface called VideoPlayer and AudioPlayer
+
+// TODO: Create instances of each implementing classes 
+
+// TODO: Call each of the methods for the objects creatively
+
